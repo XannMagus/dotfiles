f573a0a979247ab5caa0184640245c0ec6c62789
Ahmed Tedjini <ahmed@japan-experience.com>
2025-02-08 02:24:32 +0100

    [NEOVIM] Add code completion

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -7,18 +7,24 @@ return {
     },
     {
         "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "phpactor" },
-            })
-        end,
+        lazy = false,
+        opts = {
+            ensure_installed = { "lua_ls", "phpactor" },
+            auto_install = true,
+        },
     },
     {
         "neovim/nvim-lspconfig",
         config = function()
             local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({})
-            lspconfig.phpactor.setup({})
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.phpactor.setup({
+                capabilities = capabilities,
+            })
 
             vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
             vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
