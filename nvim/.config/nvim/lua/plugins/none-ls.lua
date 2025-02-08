9c2009824b7e30c876bbfb52abe1ea9d51b35341
Ahmed Tedjini <ahmed@japan-experience.com>
2025-02-08 02:23:53 +0100

    [NEOVIM] Format + add fzf native to Telescope and twigcs to none-ls

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -1,14 +1,15 @@
 return {
-    "nvimtools/none-ls.nvim",
-    config = function()
-        local null_ls = require("null-ls")
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
 
-        null_ls.setup({
-            sources = {
-                null_ls.builtins.formatting.stylua,
-                null_ls.builtins.formatting.phpcsfixer,
-                null_ls.builtins.diagnostics.phpstan,
-            },
-        })
-    end,
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.phpcsfixer,
+				null_ls.builtins.diagnostics.phpstan,
+				null_ls.builtins.diagnostics.twigcs,
+			},
+		})
+	end,
 }
