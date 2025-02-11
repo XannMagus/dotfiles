d087b37c3cb8c92c11df5caa1ee285416c9c3735
Ahmed Tedjini <ahmed.tedjini@proton.me>
2025-02-11 01:07:04 +0100

    [TMUX] Install TPM if it isn't already installed

--- tmux/install.sh
+++ tmux/install.sh
@@ -3,6 +3,17 @@
 plugins_dir="${XDG_CONFIG_HOME:-$HOME/.config}/tmux/plugins"
 catppuccin_dir="$plugins_dir/catppuccin-tmux"
 
+if [[ ! -d "$plugins_dir/tpm" ]]; then
+    print -P "%F{blue}\uf017%f Cloning TPM"
+    if ! git clone -q --depth 1 https://github.com/tmux-plugins/tpm "$plugins_dir/tpm"; then
+        print -P "%F{red}\u2716%f Failed to clone repository" >&2
+        exit 1
+    fi
+    print -P "%F{green}\u2713%f Tmux Plugin Manager installed"
+else
+    print -P "%F{green}\u2713%f TPM already installed"
+fi
+
 if [[ -d "$catppuccin_dir" ]]; then
     print -P "%F{green}\u2713 %fCatppuccin plugin already installed"
     exit 0
