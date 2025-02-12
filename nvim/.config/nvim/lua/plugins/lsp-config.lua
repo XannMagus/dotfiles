3aed34483959648f2259641f845971eeffa00719
Ahmed Tedjini <ahmed@japan-experience.com>
2025-02-12 09:56:29 +0100

    [NVIM] Use Telescope for some LSP actions

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -28,8 +28,6 @@ return {
 
             vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
             vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
-            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-            vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
 
             vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
             vim.keymap.set("n", "<leader>f", function()
--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -3,12 +3,29 @@ return {
 		"nvim-telescope/telescope.nvim",
 		tag = "0.1.8",
 		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
+		opts = {
+			pickers = {
+				buffers = {
+					initial_mode = "normal",
+				},
+			},
+		},
+		config = function(_, opts)
+			require("telescope").setup(opts)
 			local builtin = require("telescope.builtin")
 			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
 			vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
 			vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+
+			vim.keymap.set("n", "<C-k>", builtin.keymaps, { desc = "Show Keymaps" })
+
+			vim.keymap.set("n", "F", builtin.grep_string, { desc = "Find in files" })
+
+			vim.keymap.set("n", "gd", builtin.lsp_definitions, { desc = "Goto definition" })
+			vim.keymap.set("n", "gt", builtin.lsp_type_definitions, { desc = "Goto type" })
+			vim.keymap.set("n", "gu", builtin.lsp_references, { desc = "See usages" })
+			vim.keymap.set("n", "gi", builtin.lsp_implementations, { desc = "See implementations" })
 		end,
 	},
 	{
@@ -28,8 +45,8 @@ return {
 	{
 		"nvim-telescope/telescope-fzf-native.nvim",
 		build = "make",
-        config = function()
+		config = function()
 			require("telescope").load_extension("fzf")
-        end,
+		end,
 	},
 }
