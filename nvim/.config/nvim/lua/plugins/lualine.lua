7ef0f8a1a534fba4b2eb62584747925f7f8ece8e
Ahmed Tedjini <ahmed@japan-experience.com>
2025-03-10 23:20:59 +0100

    [NVIM] Shows Recording in the lualine with noice

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,10 +1,19 @@
 local options = require("vim.filetype.options")
 return {
-	"nvim-lualine/lualine.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	opts = {
-		options = {
-			theme = "catppuccin",
-		},
-	},
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+        options = {
+            theme = "catppuccin",
+        },
+        sections = {
+            lualine_x = {
+                {
+                    require("noice").api.statusline.mode.get,
+                    cond = require("noice").api.statusline.mode.has,
+                    color = { fg = "#ff9e64" },
+                },
+            },
+        },
+    },
 }
