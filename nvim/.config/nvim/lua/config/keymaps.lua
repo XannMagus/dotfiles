c0040f94d77e440e04a452ca453f4be1e5f267bc
XannMagus <ahmed.tedjini@gmail.com>
2025-03-11 00:04:07 +0100

    forgot to remove keymap linked to abandonned module

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,2 +1,1 @@
-vim.keymap.set("n", "<leader>ru", require("smart_refs").smart_references, { desc = "Smart Find References" })
 vim.keymap.set("n", "<leader>co", "<cmd>%bd|e#<CR>", { desc = "Close all other buffers" })
