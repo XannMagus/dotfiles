9c8d8b799631f50b05ebfda43537d5a4e6fa6dca
XannMagus <ahmed.tedjini@gmail.com>
2025-03-14 20:06:52 +0100

    [NVIM] Add keymaps to use system clipboard (#12)
    Co-authored-by: Ahmed Tedjini <ahmed@japan-experience.com>

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,1 +1,4 @@
 vim.keymap.set("n", "<leader>co", "<cmd>%bd|e#<CR>", { desc = "Close all other buffers" })
+vim.keymap.set("n", "<leader>++", '<cmd>let @+=@"<CR>', { desc = "Copy current yanked content to system clipboard" })
+vim.keymap.set("n", "<leader>y", '"+y', { desc = "Yank to system clipboard" })
+vim.keymap.set("n", "<leader>p", '"+p', { desc = "Paste from system clipboard" })
