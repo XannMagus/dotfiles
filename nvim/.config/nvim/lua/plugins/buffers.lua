4b20fff7d5047f0a25b23d2679eb5ef3235b7695
XannMagus <ahmed.tedjini@gmail.com>
2025-03-21 15:21:23 +0100

    [NVIM] Add better buffer navigation plugins (#17)
    Namely Harpoon and Snipe
    
    Co-authored-by: Ahmed Tedjini <ahmed@japan-experience.com>

---
+++ nvim/.config/nvim/lua/plugins/buffers.lua
@@ -0,0 +1,76 @@
+return {
+	{
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		dependencies = {
+			"nvim-telescope/telescope.nvim",
+			"nvim-lua/plenary.nvim",
+		},
+		config = function()
+			local harpoon = require("harpoon")
+
+			harpoon:setup({})
+
+			-- basic telescope configuration
+			local conf = require("telescope.config").values
+			local function toggle_telescope(harpoon_files)
+				local file_paths = {}
+				for _, item in ipairs(harpoon_files.items) do
+					table.insert(file_paths, item.value)
+				end
+
+				require("telescope.pickers")
+					.new({}, {
+						prompt_title = "Harpoon",
+						finder = require("telescope.finders").new_table({
+							results = file_paths,
+						}),
+						previewer = conf.file_previewer({}),
+						sorter = conf.generic_sorter({}),
+					})
+					:find()
+			end
+
+			vim.keymap.set("n", "<leader>a", function()
+				harpoon:list():add()
+			end)
+			vim.keymap.set("n", "<C-e>", function()
+				toggle_telescope(harpoon:list())
+			end)
+
+			vim.keymap.set("n", "<C-u>", function()
+				harpoon:list():select(1)
+			end)
+			vim.keymap.set("n", "<C-i>", function()
+				harpoon:list():select(2)
+			end)
+			vim.keymap.set("n", "<C-o>", function()
+				harpoon:list():select(3)
+			end)
+			vim.keymap.set("n", "<C-p>", function()
+				harpoon:list():select(4)
+			end)
+
+			-- Toggle previous & next buffers stored within Harpoon list
+			vim.keymap.set("n", "<C-S-P>", function()
+				harpoon:list():prev()
+			end)
+			vim.keymap.set("n", "<C-S-N>", function()
+				harpoon:list():next()
+			end)
+		end,
+	},
+	{
+		"leath-dub/snipe.nvim",
+		keys = {
+			{
+				"gb",
+				function()
+					require("snipe").open_buffer_menu()
+				end,
+				desc = "Open Snipe buffer menu",
+			},
+		},
+		opts = {},
+	},
+}
