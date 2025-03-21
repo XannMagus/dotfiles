541d9c667a0781cff68b2b3ecacb39cecbd7eaed
Ahmed Tedjini <ahmed@japan-experience.com>
2025-03-21 12:02:01 +0100

    [NVIM] Changed Lualine separators to be more inline with my Tmux configuration

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,19 +1,20 @@
-local options = require("vim.filetype.options")
 return {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    opts = {
-        options = {
-            theme = "catppuccin",
-        },
-        sections = {
-            lualine_x = {
-                {
-                    require("noice").api.statusline.mode.get,
-                    cond = require("noice").api.statusline.mode.has,
-                    color = { fg = "#ff9e64" },
-                },
-            },
-        },
-    },
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	opts = {
+		options = {
+			theme = "catppuccin",
+			section_separators = { left = "", right = "" },
+			component_separators = { left = "", right = "" },
+		},
+		sections = {
+			lualine_x = {
+				{
+					require("noice").api.status.mode.get,
+					cond = require("noice").api.status.mode.has,
+					color = { fg = "#ff9e64" },
+				},
+			},
+		},
+	},
 }
