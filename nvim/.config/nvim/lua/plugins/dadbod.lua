ec5ce444807f981c9720f640cd58a12f0170a05e
Ahmed Tedjini <ahmed@japan-experience.com>
2025-03-25 16:26:21 +0100

    [NVIM] Add keymap to display DBUI

--- nvim/.config/nvim/lua/plugins/dadbod.lua
+++ nvim/.config/nvim/lua/plugins/dadbod.lua
@@ -1,17 +1,24 @@
 return {
-    "kristijanhusak/vim-dadbod-ui",
-    dependencies = {
-        { "tpope/vim-dadbod",                     lazy = true },
-        { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true }, -- Optional
-    },
-    cmd = {
-        "DBUI",
-        "DBUIToggle",
-        "DBUIAddConnection",
-        "DBUIFindBuffer",
-    },
-    init = function()
-        -- Your DBUI configuration
-        vim.g.db_ui_use_nerd_fonts = 1
-    end,
+	"kristijanhusak/vim-dadbod-ui",
+	dependencies = {
+		{ "tpope/vim-dadbod", lazy = true },
+		{ "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true }, -- Optional
+	},
+	cmd = {
+		"DBUI",
+		"DBUIToggle",
+		"DBUIAddConnection",
+		"DBUIFindBuffer",
+	},
+	keys = {
+		{
+			"<leader>db",
+			"<cmd>DBUI<CR>",
+			desc = "[D]atabase [B]rowser",
+		},
+	},
+	init = function()
+		-- Your DBUI configuration
+		vim.g.db_ui_use_nerd_fonts = 1
+	end,
 }
