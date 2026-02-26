177018a1de0f54f3e0af1bc5f32c5a5d050dacd3
Ahmed Tedjini <ahmed@japan-experience.com>
2026-02-26 15:53:14 +0100

    make it easy to change outputs layout with a keybind

---
+++ niri/.local/bin/layout-switcher.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+CHOICE=$(echo -e "h\nw" | fuzzel \
+    --dmenu \
+    --index \
+    --auto-select \
+    --hide-prompt \
+    --config=/home/ahmed/.config/fuzzel/invisible.ini
+)
+
+case "$CHOICE" in
+    0) ln -sf "$HOME/.config/niri/outputs_home.kdl" "$HOME/.config/niri/outputs.kdl"
+    ;;
+    1) ln -sf "$HOME/.config/niri/outputs_work.kdl" "$HOME/.config/niri/outputs.kdl"
+    ;;
+    *) echo $CHOICE
+    ;;
+esac
+
